e85c24d7a89466d8eae312e7fe6ffb9f0ef40588
EvgenStr <juve2910@gmail.com>
2021-04-07 17:27:38 +0300

    add capitalize function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// Написать функцию capitalize, которая будет для каждого слова в строке делать первую букву большой.
+// Методы, которые могут понадобиться
+//   String.prototype.split
+//   Array.prototype.map
+//   String.prototype.toUpperCase
+//   String.prototype.slice
+//   Array.prototype.join
+
+const str = "some string i need";
+const capitalizeString = capitalize(str);
+
+console.log(capitalizeString);
+/**
+ * capitalize string
+ * @param {string} string 
+ * @returns {string}
+ */
+function capitalize(string) {
+  string = String.toString(string);
+  let result = string.split(' ');
+  result = result.map((text) => text[0].toUpperCase() + text.slice(1));
+  return result.join(' ');
+}
